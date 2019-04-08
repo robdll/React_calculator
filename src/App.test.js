1324cc7ed228fb3956144d53171dea7ac5305acf
koop4 <dilillo.roberto@gmail.com>
2019-04-08 17:01:00 +0200

    adds first enzyme test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+import Calculator from './components/calculator/calculator';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders a child calculator', () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.contains([
+    <Calculator />
+  ]));
 });
